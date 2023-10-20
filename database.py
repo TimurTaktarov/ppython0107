3b8fa5300a90b7696ee68aaeaabaa9bc80ff86d5
JeaLousy1337 <timurka.t.r@gmail.com>
2023-10-20 20:25:21 +0300

    datingwsqlite3 - da

--- database.py
+++ database.py
@@ -40,13 +40,64 @@ with sqlite3.connect('new_db.sqlite3') as connection:
     # """
     # cursor.executescript(query)
 
-    name = 'Max'
-    password = '123456'
-    login = 'qwerty3'
-    values = [name, login, password]
 
-    query = """
-        INSERT INTO user(name, login, password)
-        VALUES (?, ?, encode(?))
-    """
-    cursor.execute(query, values)
+    #CREATE
+    # name = 'Max2'
+    # password = '1234567'
+    # login = 'qwerty33'
+    # values = [name, login, password]
+    #
+    # query = """
+    #     INSERT INTO user(name, login, password)
+    #     VALUES (?, ?, encode(?))
+    # """
+    # cursor.execute(query, values)
+
+    # insert many
+    # values = (
+    #     ('1', '2234545', '3'),
+    #     ('1', '22rtytry34545', '3'),
+    #     ('1', '2234fgh545', '3'),
+    # )
+    #
+    # query = """
+    #     INSERT INTO user(name, login, password)
+    #     VALUES (?, ?, encode(?))
+    # """
+    # cursor.executemany(query, values)
+    #     #####################################
+
+    # READ
+    # query = """
+    #     SELECT name, address
+    #     FROM user
+    #     WHERE id >= 3
+    # """
+    # query = """
+    #     SELECT name, address
+    #     FROM user
+    #     WHERE id > 3 AND address = 'Київ'
+    # """
+    # query = """
+    #     SELECT name, address
+    #     FROM user
+    #     WHERE id > 3 AND address LIKE '%Київ%'
+    # """
+    # query = """
+    #     SELECT name, address
+    #     FROM user
+    #     WHERE id BETWEEN 3 and 6
+    # """
+    # query = """
+    #     SELECT name, address
+    #     FROM user
+    #     WHERE id = 3 OR name = '1'
+    # """
+    #
+    # result = cursor.execute(query)
+    # # print(result.fetchmany(size=3))
+    # print(result.fetchall())
+    # # print(result.fetchone())
+    # # print(result.__next__())
+    # # print(result.__next__())
+    # # print(result.__next__())
